9f0660017d1e1bca7cd47430ddaeb4556273eb7e
Kelly Merole <merok742@newschool.edu>
2014-11-03 16:32:57 -0500

    midterm finished

--- js-midterm/untitled.js
+++ js-midterm/untitled.js
@@ -2,6 +2,7 @@
 //jQuery
 var lastScroll = 0;
 var zoomLevel = 1;
+var newZoomLevel = 1;
 
 $('.container').on('scroll', function(e) {
   e.preventDefault();
@@ -9,21 +10,59 @@ $('.container').on('scroll', function(e) {
   console.log(zoomLevel);
 
   if($(this).scrollTop() > lastScroll) {
-    console.log(zoomLevel);
     //down
-    zoomLevel += Math.abs($(this).scrollTop() - lastScroll)/10;
+
+    // console.log(zoomLevel);
+    zoomLevel += Math.abs($(this).scrollTop() - lastScroll)/30;
     //$('img').css('property', 'value')
-    $('img').css('transform', 'scale(' + zoomLevel + ')');
-    //if(zoomLevel > x) {
-$('.zoomLevel').text(zoomLevel);
-    //}
-  } else if($(this).scrollTop() < lastScroll) {
-    console.log(zoomLevel);
-    //up
-    zoomLevel -= Math.abs($(this).scrollTop() - lastScroll)/10;
-    $('img').css('transform', 'scale(' + zoomLevel + ')');
-    //if(zoomLevel < x)
+
+   //when zoomed past 3
+   //show image at zoom 1
+   //show text at zoomLevel
+   //increase image zoom starting from 1 again
+
+
+    if(zoomLevel <= 2){
+        $('img').attr('src', 'power-stations.jpg');
+        $('img').css('transform', 'scale(' + zoomLevel + ')');
+    }else if(zoomLevel > 2 && zoomLevel <= 10) {
+
+        newZoomLevel += Math.abs($(this).scrollTop() - lastScroll)/30;
+        console.log("newZoomLevel: "+newZoomLevel);
+
+        $('img').attr('src', 'macbook.png');
+        $('img').css('transform', 'scale(' + zoomLevel + ')');
+
+    }else if(zoomLevel > 10){
+        $('img').attr('src', 'motherboard.jpg');
+        $('img').css('transform', 'scale(' + zoomLevel + ')');
+    }
+
+    // $('img').css('transform', 'scale(' + zoomLevel + ')');
     $('.zoomLevel').text(zoomLevel);
+
+  } else if($(this).scrollTop() < lastScroll) {
+    //up
+
+    // console.log(zoomLevel);
+    zoomLevel -= Math.abs($(this).scrollTop() - lastScroll)/30;
+    $('img').css('transform', 'scale(' + zoomLevel + ')');
+    $('.zoomLevel').text(zoomLevel);
+
+    if(zoomLevel <= 2){
+        // newZoomLevel += Math.abs($(this).scrollTop() - lastScroll)/10;
+        $('img').attr('src', 'power-stations.jpg');
+        // $('img').css('transform', 'scale(' + newZoomLevel + ')');
+
+    }else if(zoomLevel > 2 && zoomLevel <= 10) {
+        // newZoomLevel += Math.abs($(this).scrollTop() - lastScroll)/10;
+        $('img').attr('src', 'macbook.png');
+        // $('img').css('zoom', '10%');
+
+    }else if(zoomLevel > 10){
+        $('img').attr('src', 'motherboard.jpg');
+
+    }
   }
 
   lastScroll = $(this).scrollTop();
@@ -32,7 +71,7 @@ $('.zoomLevel').text(zoomLevel);
 
 var numbers = console.log(zoomLevel);
 
-$('<span>numbers</span>').appendTo('div.output');
+// $('<span>numbers</span>').appendTo('div.output');
 //class
 // put in container
 });
